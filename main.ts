6df0aaa8fe476baa4850203d1086039a5e788eb8
Juilio <143953362+Juilio@users.noreply.github.com>
2024-04-24 22:31:12 +0200

    Update main.ts
    First test for adding german

--- main.ts
+++ main.ts
@@ -403,6 +403,77 @@ export default class BibleLinkerPro extends Plugin {
 					["ju", "judas"],
 					["ap", "apo", "apocalipse"],
 				];
+				
+				const bibleBooksGer = [
+					["1mo", "1moses"],
+					["2mo", "2moses"],
+					["3mo", "3moses"],
+					["4mo", "4moses"],
+					["5mo", "5moses"],
+					["jos", "josua"],
+					["ri", "richter"],
+					["ru", "ruth"],
+					["1sam", "1samuel"],
+					["2sam", "2samuel"],
+					["1kö", "1könige"],
+					["2kö", "2könige"],
+					["1chr", "1chronika"],
+					["2chr", "2chronika"],
+					["es", "esra"],
+					["neh", "nehemia"],
+					["esth", "esther"],
+					["hi", "hiob"],
+					["ps", "psalmen"],
+					["spr", "sprüche"],
+					["pred", "prediger"],
+					["hoh", "hohes lied"],
+					["jes", "jesaja"],
+					["jer", "jeremia"],
+					["klag", "klagelieder"],
+					["hes", "hesekiel"],
+					["dan", "daniel"],
+					["hos", "hosea"],
+					["joe", "joel"],
+					["am", "amos"],
+					["ob", "obadja"],
+					["jon", "jona"],
+					["mi", "micha"],
+					["nah", "nahum"],
+					["hab", "habakuk"],
+					["zeph", "zephanja"],
+					["hag", "haggai"],
+					["sach", "sacharja"],
+					["mal", "maleachi"],
+					["mat", "matthäus"],
+					["mar", "markus"],
+					["luk", "lukas"],
+					["joh", "johannes"],
+					["apg", "apostelgeschichte"],
+					["röm", "römer"],
+					["1kor", "1korinther"],
+					["2kor", "2korinther"],
+					["gal", "galater"],
+					["eph", "epheser"],
+					["phil", "philipper"],
+					["kol", "kolosser"],
+					["1thes", "1thessalonicher"],
+					["2thes", "2thessalonicher"],
+					["1tim", "1timotheus"],
+					["2tim", "2timotheus"],
+					["tit", "titus"],
+					["phi", "philem", "philemon"],
+					["heb", "hebräer"],
+					["jak", "jakobus"],
+					["1pet", "1petrus"],
+					["2pet", "2petrus"],
+					["1joh", "1johannes"],
+					["2joh", "2johannes"],
+					["3joh", "3johannes"],
+					["jud", "judas"],
+					["offb", "offenbarung"],
+					
+					
+					
 
 				let bibleBooks = bibleBooksEN;
 
@@ -412,6 +483,8 @@ export default class BibleLinkerPro extends Plugin {
 					bibleBooks = bibleBooksFR;
 				} else if (this.settings.pluginLanguage == "pt-br") {
 					bibleBooks = bibleBooksPtBr;
+				}else if (this.settings.pluginLanguage == "ger") {
+					bibleBooks = bibleBooksGer;
 				}
 
 				let linkOutput = "";
